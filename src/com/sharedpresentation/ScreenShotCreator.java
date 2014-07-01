5b472984838590de08a937886bf4a8bded342b7d
alshevchuk <NeoNode-N1@yandex.ru>
2014-07-01 20:39:19 +0300

    Add new control window

--- src/com/sharedpresentation/ScreenShotCreator.java
+++ src/com/sharedpresentation/ScreenShotCreator.java
@@ -1,12 +1,5 @@
 package com.sharedpresentation;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,25 +8,32 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.RectangleBuilder;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Admin on 16.06.14.
  */
 public class ScreenShotCreator extends Application {
-//    private static String path_to_image = "D:\\Java\\wildfly-8.1.0.Final\\wildfly-8.1.0.Final\\res\\";
-    private static String path_to_image = "D:\\Java\\TomCats\\apache-tomcat-8.0.8\\res\\";
-    private int sX = -1, fX = -1;
-    private int sY = -1, fY = -1;
-    protected boolean firstData = true;
-    protected boolean secondData = false;
+    //    private static String path_to_image = "D:\\Java\\wildfly-8.1.0.Final\\wildfly-8.1.0.Final\\res\\";
+//    private static String path_to_image = "D:\\Java\\TomCats\\apache-tomcat-8.0.8\\res\\";
+    private static String path_to_image = "D:\\java-data\\apache-tomcat-8.0.8\\apache-tomcat-8.0.8\\res\\";
+    private int start_X = -1, final_X = -1;
+    private int start_Y = -1, final_Y = -1;
+    protected boolean firstCoordinats = true;
+    protected boolean secondCoordinats = false;
     private int height = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
     private int width = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
     private Timer timer2 = null;
@@ -41,59 +41,56 @@ public class ScreenShotCreator extends Application {
     private static int imgCounter = 0;
 
     public static void main(String[] args) {
-//		System.out.println(java.awt.MouseInfo.getPointerInfo().getLocation().x);
-
+        //TODO: make file transfer via socket
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.initStyle(StageStyle.TRANSPARENT);
+        Stage control = new Stage();
 
-        Group root = new Group();
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setOpacity(0.1f);
 
-        final Button b = new Button("set start coordinat");
-        b.setOnAction(new EventHandler<ActionEvent>() {
+        Group rootGroup = new Group();
+
+        EventHandler<ActionEvent> setCoordinatEvent = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent arg0) {
-                if (sX == -1 || fX == -1) {
-                    b.setDisable(true);
-                }
-                if (sX != -1 || fX != -1) {
-                    b.setDisable(false);
-                }
+                firstCoordinats = true;
+                secondCoordinats = true;
             }
 
-        });
+        };
+        final Button setCoordinat = createButton("set start coordinat", setCoordinatEvent);
 
-        final Button b1 = new Button("start getting jpg...");
-        b1.setOnAction(new EventHandler<ActionEvent>() {
+        EventHandler<ActionEvent> startGettingPictureEvent = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent arg0) {
                 // start timer
                 timer2 = new java.util.Timer();
                 task = new TimerTask() {
                     public void run() {
-                        getScreen(sX, sY, fX, fY);
+                        getScreen(start_X, start_Y, final_X, final_Y);
                     }
                 };
                 timer2.schedule(task, 0, 500);
             }
+        };
+        final Button startGettingPicture = createButton("start getting jpg...", startGettingPictureEvent);
 
-        });
-
-        final Button b2 = new Button("stop getting jpg...");
-        b2.setOnAction(new EventHandler<ActionEvent>() {
+        EventHandler<ActionEvent> stopGettingPictureEvent = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent arg0) {
                 //stop timer
-                timer2.cancel();
+                if (timer2 != null)
+                    timer2.cancel();
             }
+        };
+        final Button stopGettingPicture = createButton("stop getting jpg...", stopGettingPictureEvent);
 
-        });
-
-        HBox h = new HBox();
-        h.getChildren().addAll(b, b1, b2);
+        VBox vBox = createVBox();
+        vBox.getChildren().addAll(setCoordinat, startGettingPicture, stopGettingPicture);
 
         javafx.scene.shape.Rectangle rect2 = RectangleBuilder.create()
                 .arcWidth(30).arcHeight(30).fill(Color.color(0, 1, 1, 0.01)).x(10).y(160)
@@ -105,38 +102,57 @@ public class ScreenShotCreator extends Application {
         v.getChildren().add(rect2);
 
         GridPane t = new GridPane();
-        t.add(h, 1, 1);
+        t.add(vBox, 1, 1);
         t.add(v, 1, 2);
 
-        root.getChildren().add(t);
+        rootGroup.getChildren().add(t);
 
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(new Group(), 800, 600);
         scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent m) {
-                if (firstData) {
-                    sX = (int) m.getScreenX();
-                    sY = (int) m.getScreenY();
-                    System.out.println("click1!" + sX + " , " + sY);
-                    firstData = false;
-                    secondData = true;
+                if (firstCoordinats) {
+                    start_X = (int) m.getScreenX();
+                    start_Y = (int) m.getScreenY();
+                    System.out.println("click1!" + start_X + " , " + start_Y);
+                    firstCoordinats = false;
+                    secondCoordinats = true;
                     return;
                 }
 
-                if (secondData) {
-                    fX = (int) m.getScreenX();
-                    fY = (int) m.getScreenY();
-                    System.out.println("click2!" + fX + " , " + fY);
-                    firstData = true;
-                    secondData = false;
+                if (secondCoordinats) {
+                    final_X = (int) m.getScreenX();
+                    final_Y = (int) m.getScreenY();
+                    System.out.println("click2!" + final_X + " , " + final_Y);
+                    firstCoordinats = false;
+                    secondCoordinats = false;
                 }
             }
         });
         scene.setFill(Color.TRANSPARENT);
-
+        control.setScene(new Scene(rootGroup, 200, 100));
+        control.show();
+        control.setX(scene.getX() / 2);
+        control.setY(scene.getY());
         stage.setScene(scene);
-        stage.setFullScreen(true);
+        stage.setFullScreen(false);
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
         stage.show();
+    }
+
+    public VBox createVBox() {
+        return new VBox();
+    }
+
+    public Button createButton(String name, EventHandler<ActionEvent> f) {
+        Button b = new Button(name);
+        b.setOnAction(f);
+        return b;
     }
 
     public static void getScreen(double xs, double ys, double xf, double yf) {
@@ -144,7 +160,6 @@ public class ScreenShotCreator extends Application {
         screenRect.setBounds((int) xs, (int) ys, ((int) (xf - xs)), ((int) (yf - ys)));
         try {
             BufferedImage bufferedImage = new Robot().createScreenCapture(screenRect);
-//            ImageIO.write(bufferedImage, "jpg", new File("screens/image" + ".jpg"));
             ImageIO.write(bufferedImage, "jpg", new File(path_to_image + "image" + ".jpg"));
             Thread.sleep(500);
         } catch (AWTException e) {
@@ -159,9 +174,4 @@ public class ScreenShotCreator extends Application {
 
     }
 
-    public <T, K, M> T sum(T item1, K item2) {
-
-        return item1;
-
-    }
 }
